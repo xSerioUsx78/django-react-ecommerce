088d84904b5366e8bf642b621af2590c291e00c3
weird <lomililmore@gmail.com>
2021-06-24 18:23:20 +0430

    FIRST COMMIT

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -144,6 +144,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
