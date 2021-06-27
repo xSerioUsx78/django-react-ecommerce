f60ee4ae85882c610984de7aab38ceade886ec00
weird <lomililmore@gmail.com>
2021-06-27 19:10:34 +0430

    OK

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -18,13 +18,14 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import ReactAppView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('product/', include('product.urls', namespace='product')),
     path('order/', include('order.urls', namespace='order')),
-    re_path(".*", ReactAppView.as_view())
+    re_path(".*", TemplateView.as_view(template_name="index.html"))
 ]
 
 if settings.DEBUG:
